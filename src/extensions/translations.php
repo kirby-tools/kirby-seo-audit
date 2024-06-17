4c993d559a4eb77b29289cd4f597b1146f300f11
Johann Schopplich <mail@johannschopplich.com>
2024-06-17 15:31:37 +0200

    chore: update translations

--- src/extensions/translations.php
+++ src/extensions/translations.php
@@ -10,7 +10,7 @@ return [
         'johannschopplich.seo-audit.analyze.error' => 'Could not generate report. Please check console logs.',
         'johannschopplich.seo-audit.error.missingPreviewUrl' => 'The "previewUrl" is disabled in the blueprint, but is required for the SEO analysis.',
         'johannschopplich.seo-audit.license.buy' => 'Buy a license',
-        'johannschopplich.seo-audit.license.activate' => 'Activate',
+        'johannschopplich.seo-audit.license.activate' => 'Activate'
     ],
     'de' => [
         'johannschopplich.seo-audit.label' => 'SEO-Audit',
@@ -21,6 +21,6 @@ return [
         'johannschopplich.seo-audit.analyze.error' => 'Bericht konnte nicht generiert werden. Bitte Konsolenprotokolle prüfen.',
         'johannschopplich.seo-audit.error.missingPreviewUrl' => 'Die "previewUrl" ist im Blueprint deaktiviert, wird aber für die SEO-Analyse benötigt.',
         'johannschopplich.seo-audit.license.buy' => 'Lizenz kaufen',
-        'johannschopplich.seo-audit.license.activate' => 'Aktivieren',
+        'johannschopplich.seo-audit.license.activate' => 'Aktivieren'
     ]
 ];
