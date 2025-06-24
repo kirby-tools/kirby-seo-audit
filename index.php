2baf9f7ec7bf048ac003987799af195b78f82544
Johann Schopplich <mail@johannschopplich.com>
2025-06-24 17:39:34 +0200

    feat: custom Kirby 5 plugin license status

--- index.php
+++ index.php
@@ -4,8 +4,23 @@ use Kirby\Cms\App as Kirby;
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-Kirby::plugin('johannschopplich/seo-audit', [
-    'api' => require __DIR__ . '/src/extensions/api.php',
-    'sections' => require __DIR__ . '/src/extensions/sections.php',
-    'translations' => require __DIR__ . '/src/extensions/translations.php'
-]);
+$pluginConfig = [
+    'name' => 'johannschopplich/seo-audit',
+    'extends' => [
+        'api' => require __DIR__ . '/src/extensions/api.php',
+        'sections' => require __DIR__ . '/src/extensions/sections.php',
+        'translations' => require __DIR__ . '/src/extensions/translations.php'
+    ]
+];
+
+if (class_exists('\Kirby\Plugin\License')) {
+    Kirby::plugin(
+        ...$pluginConfig,
+        license: fn ($plugin) => new \JohannSchopplich\Licensing\PluginLicense(
+            plugin: $plugin,
+            packageName: 'johannschopplich/kirby-seo-audit'
+        )
+    );
+} else {
+    Kirby::plugin(...$pluginConfig);
+}
