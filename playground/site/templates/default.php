5c6c7a599bcae05556b644c1252d438a4c86d9f5
Johann Schopplich <johann@schopplich.com>
2026-09-16 10:08:17 +0200

    refactor: reorganize PHP opening tags and variable declarations

--- playground/site/templates/default.php
+++ playground/site/templates/default.php
@@ -1,3 +1,10 @@
+<?php
+
+/** @var \Kirby\Cms\App $kirby */
+/** @var \Kirby\Cms\Site $site */
+/** @var \Kirby\Cms\Page $page */
+
+?>
 <!DOCTYPE html>
 <html lang="<?= $kirby->language()->code() ?>">
 <head>
