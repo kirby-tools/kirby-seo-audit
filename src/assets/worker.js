8a06325c1b190cd94192f20853820f775446b69f
Johann Schopplich <johann@schopplich.com>
2026-09-16 12:56:28 +0200

    refactor: use the Yoast researcher factory

--- src/assets/worker.js
+++ src/assets/worker.js
@@ -5,57 +5,7 @@ import {
   helpers,
   languageProcessing,
 } from "./yoastseo-repo/packages/yoastseo/src/index.js";
-import DefaultResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/_default/Researcher";
-import ArabicResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/ar/Researcher";
-import CatalanResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/ca/Researcher";
-import CzechResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/cs/Researcher";
-import GermanResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/de/Researcher";
-import GreekResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/el/Researcher";
-import EnglishResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/en/Researcher";
-import SpanishResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/es/Researcher";
-import FarsiResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/fa/Researcher";
-import FrenchResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/fr/Researcher";
-import HebrewResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/he/Researcher";
-import HungarianResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/hu/Researcher";
-import IndonesianResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/id/Researcher";
-import ItalianResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/it/Researcher";
-import JapaneseResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/ja/Researcher";
-import NorwegianResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/nb/Researcher";
-import DutchResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/nl/Researcher";
-import PolishResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/pl/Researcher";
-import PortugueseResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/pt/Researcher";
-import RussianResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/ru/Researcher";
-import SlovakResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/sk/Researcher";
-import SwedishResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/sv/Researcher";
-import TurkishResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/languages/tr/Researcher";
-
-/// keep-sorted
-const researchers = {
-  ar: ArabicResearcher,
-  ca: CatalanResearcher,
-  cs: CzechResearcher,
-  de: GermanResearcher,
-  el: GreekResearcher,
-  en: EnglishResearcher,
-  es: SpanishResearcher,
-  fa: FarsiResearcher,
-  fr: FrenchResearcher,
-  he: HebrewResearcher,
-  hu: HungarianResearcher,
-  id: IndonesianResearcher,
-  it: ItalianResearcher,
-  ja: JapaneseResearcher,
-  nb: NorwegianResearcher,
-  nl: DutchResearcher,
-  pl: PolishResearcher,
-  pt: PortugueseResearcher,
-  ru: RussianResearcher,
-  sk: SlovakResearcher,
-  sv: SwedishResearcher,
-  tr: TurkishResearcher,
-};
-
-const researchersMap = new Map(Object.entries(researchers));
+import getResearcher from "./yoastseo-repo/packages/yoastseo/src/languageProcessing/getResearcher";
 
 // Important: `AnalysisWebWorker` strictly expects the `onmessage` function as part of the scope.
 self.onmessage = (event) => {
@@ -67,16 +17,6 @@ self.onmessage = (event) => {
 
   worker.register();
 };
-
-function getResearcher(language) {
-  if (researchersMap.has(language)) {
-    if (typeof researchersMap.get(language) === "function") {
-      return researchersMap.get(language);
-    }
-  }
-
-  return DefaultResearcher;
-}
 
 function registerPremiumAssessments(worker, language) {
   const {
